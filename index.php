7021aeb8178d3a453e5c27b315233d313885445e
Adexdee007 <dammyfor2020@gmail.com>
2025-01-31 12:12:59 +0100

    HNG12_stage0_task

--- index.php
+++ index.php
@@ -16,6 +16,7 @@ $response = [
     "email" => "dammyfor2020@gmail.com",  // Replace with your actual email
     "current_datetime" => gmdate("c"),   // Generates ISO 8601 format in UTC
     "github_url" => "https://github.com/Adexdee007/HNG12_stage0_public_api"
+    
 ];
 
 // Return the response as JSON
