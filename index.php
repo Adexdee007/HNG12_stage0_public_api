b1ef5ac752b5bc002895b25f2aaeb051e606c322
Adexdee007 <dammyfor2020@gmail.com>
2025-01-31 22:08:19 +0100

    HNG12_stage0_task

--- index.php
+++ index.php
@@ -8,13 +8,13 @@ header('Access-Control-Allow-Origin: *');
 
 // Function to get the current datetime in ISO 8601 format
 function getCurrentDateTime() {
-    return gmdate('Y-m-d\\TH:i:s\\Z');
+    return gmdate("Y-m-d\\TH:i:s\\Z");
 }
 
 // Defining the response
 $response = [
     "email" => "dammyfor2020@gmail.com",  // Replace with your actual email
-    "current_datetime" => gmdate("c"),   // Generates ISO 8601 format in UTC
+    "current_datetime" => getCurrentDateTime(),   // Generates ISO 8601 format in UTC
     "github_url" => "https://github.com/Adexdee007/HNG12_stage0_public_api"
     
 ];
